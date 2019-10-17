0e28b0e2158b3c6e284a5847c551470fd9705ff9
Marcos Wendell Santos <mwsantos_token@gmail.com>
2019-10-17 17:12:51 -0300

    Change in dist files for db connection

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -9,12 +9,5 @@ module.exports = {
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: ['src/entity/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
-  },
+  entities: ['dist/entity/**/*.entity.js'],
 };
